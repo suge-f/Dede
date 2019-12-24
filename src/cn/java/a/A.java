5127e8e4995812068cc272556515377480bb176c
suge <1220917050@qq.com>
2019-12-24 14:42:45 +0800

    D

---
+++ src/cn/java/a/A.java
@@ -0,0 +1,26 @@
+package cn.java.a;
+
+public class A {
+
+	public static void main(String[] args) {
+
+		System.out.println("aaa");
+		System.out.println("ccc");
+		System.out.println("eee");
+		System.out.println("dada");
+
+	}
+
+	public String getA() {
+		System.out.println("sss");
+
+		return "A";
+	}
+
+	public String getB() {
+		System.out.println("sss");
+
+		return "A";
+	}
+
+}
