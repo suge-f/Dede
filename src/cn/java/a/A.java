fb93e28132cf0c9226ae68882302e8c7ffdd693e
suge <1220917050@qq.com>
2019-12-24 14:43:52 +0800

    D

--- src/cn/java/a/A.java
+++ src/cn/java/a/A.java
@@ -4,7 +4,7 @@ public class A {
 
 	public static void main(String[] args) {
 
-		System.out.println("aaa");
+		System.out.println("CCCC");
 		System.out.println("ccc");
 		System.out.println("eee");
 		System.out.println("dada");
